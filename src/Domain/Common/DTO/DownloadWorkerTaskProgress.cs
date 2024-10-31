9b43e447e4312efbb1671841cfef79d0ac7f7b60
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-10-31 15:46:56 +0100

    refactor(WebAPI): Added progress for DownloadWorkerTask

--- src/Domain/Common/DTO/DownloadWorkerTaskProgress.cs
+++ src/Domain/Common/DTO/DownloadWorkerTaskProgress.cs
@@ -1,9 +1,12 @@
 namespace PlexRipper.Domain;
 
-public record DownloadTaskProgress : IDownloadTaskProgress
+public record DownloadWorkerTaskProgress : IDownloadTaskProgress
 {
     public long DataTotal { get; set; }
+
     public decimal Percentage { get; set; }
+
     public long DataReceived { get; set; }
+
     public long DownloadSpeed { get; set; }
 }
