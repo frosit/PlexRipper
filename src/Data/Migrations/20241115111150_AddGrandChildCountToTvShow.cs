871981a66b71755a56958265d3def07d919b5c98
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-11-15 12:12:39 +0100

    build(WebAPI): added GrandChildCount to PlexTvShow migration

---
+++ src/Data/Migrations/20241115111150_AddGrandChildCountToTvShow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGrandChildCountToTvShow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GrandChildCount",
+                table: "PlexTvShows",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrandChildCount",
+                table: "PlexTvShows");
+        }
+    }
+}
